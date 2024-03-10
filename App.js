719534151d515cf0c2b4d2c435234af4591a5598
Venkat Aravind <150166142+VenkatAravind123@users.noreply.github.com>
2024-03-10 10:52:54 +0530

    v1

---
+++ App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import {BrowserRouter as Router} from "react-router-dom"
+// import MainNavBar from "./main/MainNavBar";
+import DashBoard from './main/DashBoard';
+function App() {
+  return (
+    <div className="App">
+      
+      {/* <Router>
+        <MainNavBar/>
+      </Router> */}
+      <DashBoard/>
+    </div>
+  );
+}
+
+export default App;
